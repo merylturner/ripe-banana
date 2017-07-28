0b978410cba2271dc56a32b61a6206ce4183ca70
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-07-28 12:19:21 -0700

    in progress with the review test not catching the enum validation properly

--- tests/unit/review.test.js
+++ tests/unit/review.test.js
@@ -1,34 +1,70 @@
-const {assert} = require('chai');
+const { assert } = require('chai');
 const Review = require('../../lib/models/review');
 const Reviewer = require('../../lib/models/reviewer');
 const Film = require('../../lib/models/film');
 
 describe('Review Model unit tests', () => {
     it('validates the review model required fields', () => {
-        const reviewer = new Reviewer ({name: 'Marty Baller', company: 'Balling Corporation'});
-        const film = new Film({ 
-            title: 'Finding Nemo', 
+        const reviewer = new Reviewer({ name: 'Marty Baller', company: 'Balling Corporation' });
+        const film = new Film({
+            title: 'Finding Nemo',
             studio: '123456789009876543211234',
-            released: 2003, 
+            released: 2003,
             cast: [
                 { role: 'Nemo', actor: '123456789098765432123456' },
                 { role: 'Dory', actor: '123456789098765432123456' },
-                { role: 'Marlin', actor: '123456789098765432123456' }              
-                ]
-            });
-        const review = new Review ({
-          rating: 2, 
-          reviewer: reviewer._id, 
-          review: 'It was aight!!!!', 
-          film: film._id, 
-          createdAt: new Date,
-          updatedAt: new Date 
+                { role: 'Marlin', actor: '123456789098765432123456' }
+            ]
+        });
+        const review = new Review({
+            rating: 2,
+            reviewer: reviewer._id,
+            review: 'It was aight!!!!',
+            film: film._id,
+            createdAt: new Date,
+            updatedAt: new Date
         });
         return review.validate();
     });
 
-    it.skip('fails validation when required fields are missing', () => {
+    it('fails validation when required fields are missing', () => {
         const review = new Review();
+        return review.validate()
+            .then(
+            () => { throw new Error('Expected validation error'); },
+            ({ errors }) => {
+                assert.ok(errors.rating.kind);
+                assert.ok(errors.reviewer.kind);
+                assert.ok(errors.review.kind);
+                assert.ok(errors.film.kind);
 
+            });
+    });
+    it('should be an enum type', () => {
+        const reviewer = new Reviewer({ name: 'Marty Baller', company: 'Balling Corporation' });
+        const film = new Film({
+            title: 'Finding Nemo',
+            studio: '123456789009876543211234',
+            released: 2003,
+            cast: [
+                { role: 'Nemo', actor: '123456789098765432123456' },
+                { role: 'Dory', actor: '123456789098765432123456' },
+                { role: 'Marlin', actor: '123456789098765432123456' }
+            ]
+        });
+        const review = new Review({
+            rating: 2,
+            reviewer: reviewer._id,
+            review: 'It was aight!!!!',
+            film: film._id,
+            createdAt: new Date,
+            updatedAt: new Date
+        });
+        return review.validate()
+            .then(
+            () => { throw new Error('Expected validation error'); },
+            ({ errors }) => {
+                assert.equal(errors.rating.kind, 'enum');
+            });
     });
 });
