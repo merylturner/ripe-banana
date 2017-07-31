0bc5bdb5877cf90d7408a0259f1b83f8e20a9bfd
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-07-31 14:55:08 -0700

    first test passes

--- tests/e2e/studios.test.js
+++ tests/e2e/studios.test.js
@@ -1,49 +1,71 @@
 const db = require('./_db');
+const chai = require('chai');
 const request = require('./_request');
 const { assert } = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
 describe('studios REST api', () => {
     before(db.drop);
 
     const warner = {
-        name: 'Warner Bros Studios', 
-        location: 'Los Angeles'
+        name: 'Warner Bros Studios',
+        address: {
+            city: 'Los Angeles',
+            state: 'California',
+            country: 'USA'
+        }
     };
 
     const fox = {
-        name: 'Fox Studios', 
-        location: 'New York'
-    };
-
+        name: 'Fox Studios',
+        address: {
+            city: 'New York',
+            state: 'Maryland',
+            country: 'USA'
+        }
+    }
     const universal = {
         name: 'Universal Studios',
-        location: 'Portland'
-    };
+        address: {
+            city: 'Detroit',
+            state: 'Georgia',
+            country: 'USA'
+        }
+    }
 
     function saveStudio(studio) {
-        return request.post('/api/studios')
+        return request.post('/studios')
             .send(studio)
-            .then(( { studio }) => {
+            .then(({ body }) => {
                 studio._id = body._id;
                 studio._v = body._v;
+                studio.name = body.name;
                 return body
             });
     }
-    
-
-    it.only('GETs studio if it exists', () => {
-        return request
-            .get('/api/studios/`${warner._id}')
-            .then(req => res.body)
-            .then(studio => {
-                assert.equal(studio.name, warner.name);
-                assert.equal(studio.location, warner.location);
-            });          
+    it.only('saves a studio', () => {
+        return saveStudio(warner)
+            .then(savedStudio => {
+                assert.ok(savedStudio._id);
+                assert.isOk(savedStudio.name)
+                assert.deepEqual(savedStudio.name, warner.name);
+                assert.deepEqual(savedStudio.address, warner.address);
+            });
     });
 
-it('returns 404 if otter does not exist', () => {
-        return request.get('/api/studios/58ff9f496aafd447111c29b5')
-        .then(
+    it.only('GETs a studio if it exists', () => {
+        return request.get(`/studios/${warner._id}`)
+            .then(res => res.body)
+            .then(studio => {
+                assert.equal(studio.name, warner.name);
+                assert.equal(studio.address, warner.address);
+            });
+    });
+
+    it('returns 404 if otter does not exist', () => {
+        return request.get('/studios/58ff9f496aafd447111c29b5')
+            .then(
             () => {
                 throw new Error('Unexpected Success In Error Test');
             },
@@ -51,26 +73,34 @@ it('returns 404 if otter does not exist', () => {
                 assert.equal(res.status, 404);
                 assert.equal(res.message, 'Not Found');
             }
-        );
+            );
     });
 
     it('GET all Studios', () => {
         return Promise.all([
             saveStudio(warner),
-            saveStudio(Fox),
+            saveStudio(fox),
         ])
-            .then(() => request.get('/api/studios'))
+            .then(() => request.get('/studios'))
             .then(res => {
                 const studios = res.body;
                 assert.equal(studios[2].name, warner.name);
-                assert.equal(studios[1].type, fox.location);
+                assert.equal(studios[1].address, fox.address);
             });
     });
 
-    it('Replaces otter content', () => {
-        const marvel = { name: 'Marvel Studios', location: 'Mars'};
+    it('Replaces studio content', () => {
+        const marvel = {
+            name: 'Marvel Studios',
+            address: {
+                city: 'Portland',
+                state: 'Oregon',
+                country: 'USA'
+            }
+        };
+
         return request
-            .put(`/api/studios/${warner._id}`)
+            .put(`/studios/${warner._id}`)
             .send(marverl)
             .then(res => res.body)
             .then(studio => {
@@ -79,11 +109,11 @@ it('returns 404 if otter does not exist', () => {
             });
     });
 
-    it('deletes an studio by id',() => {
+    it('deletes an studio by id', () => {
         return request
-            .delete(`/api/studios/${warner._id}`)
+            .delete(`/studios/${warner._id}`)
             .then(res => {
-                assert.deepEqual(JSON.parse(res.text), {removed: true});
+                assert.deepEqual(JSON.parse(res.text), { removed: true });
             });
     });
 });
