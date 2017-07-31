d6e38f1749ee938c654d85fbf74bd9bddbdd62ca
Meryl Turner <merylturner08@gmail.com>
2017-07-31 09:54:25 -0700

    finished film unit testing

--- tests/unit/film.test.js
+++ tests/unit/film.test.js
@@ -4,11 +4,12 @@ const Actor = require('../../lib/models/actor');
 const Studio = require('../../lib/models/studio');
 
 describe('Film model unit tests', () => {
+    const actorOne = new Actor({ name: 'Tina Fey' });
+    const actorTwo = new Actor({ name: 'Brad Pitt' });
+    const actorThree = new Actor({ name: 'Amy Poehler' });
+    const studio = new Studio({ name: 'Sarah Joy Studios' });
+
     it('validates with required fields', () => {
-        const actorOne = new Actor({ name: 'Tina Fey' });
-        const actorTwo = new Actor({ name: 'Brad Pitt' });
-        const actorThree = new Actor({ name: 'Amy Poehler' });
-        const studio = new Studio({ name: 'Sarah Joy Studios' });
         const film = new Film({
             title: 'Mean Girls',
             studio: studio._id,
@@ -22,8 +23,35 @@ describe('Film model unit tests', () => {
         return film.validate();
     });
 
-//TODO: what other validations do we need for the film unit tests?
+    it('fails validation', () => {
+        const film = new Film();
+        return film.validate()
+            .then(() => { throw new Error('expected validation error'); },
+                ({ errors }) => {
+                //TODO: Cast array property  
+                    assert.ok(errors.title.kind);
+                    assert.ok(errors.released.kind);
+                    assert.ok(errors.studio.kind);
+                });
+    });
 
+    it('checks if released is a number', () => {
+        const film = new Film({
+            title: 'Mean Girls',
+            studio: studio._id,
+            released: 'fake',
+            cast: [
+                { role: 'Miss Norbury', actor: actorOne._id },
+                { role: 'Cady Heron', actor: actorTwo._id },
+                { role: 'Regina George', actor: actorThree._id }
+            ]
+        });
+        return film.validate()
+            .then(() => { throw new Error('expected validation error');},
+                ({ errors }) => {
+                    assert.equal(errors.released.kind, 'Number');
+                });
+    });
 
 
 });
